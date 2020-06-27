3ed24c2cabf3a9329d2a7f772f7c1d06ba33a74d
JohnnyWong-hub <1426384536@qq.com>
2020-06-27 21:31:27 +0800

    git push

--- main.c
+++ main.c
@@ -14,9 +14,9 @@ int SaveInformation();
 
 void MainMenu()
 {
-  printf(":--------------------------------------------------:\n");
+	printf(":--------------------------------------------------:\n");
 	printf("                  Booking Tickets                  :\n");
-  printf(":--------------------------------------------------:\n");
+	printf(":--------------------------------------------------:\n");
 	printf(":          0:Quit the system                       :\n");
 	printf(":          1:Insert a train information            :\n");
 	printf(":          2:Serach a train information            :\n");
@@ -24,89 +24,86 @@ void MainMenu()
 	printf(":          4:Modify the train information          :\n");
 	printf(":          5:Show the train information            :\n");
 	printf(":          6:Save information to file              :\n");
-  printf(":--------------------------------------------------:\n");
-  printf("      please choose <0~6>:                            ");
+	printf(":--------------------------------------------------:\n");
+	printf("      please choose <0~6>:                            ");
 }
 
 void MenuChoose(int mychoose)
 {
-  switch (mychoose)
+	switch (mychoose)
 	{
-	  case 0:
-      QuitSystem();
-			break;
-    case 1:
-		  InsertInformation();
-			break;
-    case 2:
-		  SerachInformation();
-			break;
-    case 3:
-		  BookTicket();
-			break;
-    case 4:
-		  ModifyInformation();
-			break;
-    case 5:
-		  ShowInformation();
-			break;
-    case 6:
-		  SaveInformation();
-			break;
-    default:
-		  break;
+	case 0:
+		QuitSystem();
+		break;
+	case 1:
+		InsertInformation();
+		break;
+	case 2:
+		SerachInformation();
+		break;
+	case 3:
+		BookTicket();
+		break;
+	case 4:
+		ModifyInformation();
+		break;
+	case 5:
+		ShowInformation();
+		break;
+	case 6:
+		SaveInformation();
+		break;
+	default:
+		break;
 	}
 }
 
 int QuitSystem()
 {
-  return 0;
+	return 0;
 }
 
 int InsertInformation()
 {
-  return 0;
+	return 0;
 }
 
 int SerachInformation()
 {
-  return 0;
+	return 0;
 }
 
 int BookTicket()
 {
-  return 0;
+	return 0;
 }
 
 int ModifyInformation()
 {
-  return 0;
+	return 0;
 }
 
 int ShowInformation()
 {
-  return 0;
+	return 0;
 }
 
 int SaveInformation()
 {
-  return 0;
+	return 0;
 }
 
 int main(int argc, const char *argv[])
 {
 	int choose = -1;
-	while(1)
+	while (1)
 	{
-	  MainMenu();
-		scanf("%d",&choose);
-		while(getchar() != '\n')
+		MainMenu();
+		scanf("%d", &choose);
+		while (getchar() != '\n')
 		{
 		}
-	  MenuChoose(choose);
+		MenuChoose(choose);
 	}
 	return 0;
 }
-
-
-
